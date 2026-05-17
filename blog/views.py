5f9f324322706ee6fca099da3717cca43dffaf9b
Leonid Stoyko <gribojor@gmail.com>
2026-05-17 23:26:37 +0500

    reduce db queries number.

--- blog/views.py
+++ blog/views.py
@@ -41,14 +41,16 @@ def serialize_tag(tag):
 def index(request):
 
     most_fresh_posts = (
-        Post.objects.prefetch_related('author')
-        .annotate(comments_count=Count('comments'))
+        Post.objects.annotate(comments_count=Count('comments'))
         .order_by('-published_at')[:5]
+        .prefetch_related('author')
+        .prefetch_related('tags')
     )
 
     most_popular_posts = (
         Post.objects.popular()
         .prefetch_related('author')[:5]
+        .prefetch_related('tags')
         .fetch_with_comments_count()
     )
 
