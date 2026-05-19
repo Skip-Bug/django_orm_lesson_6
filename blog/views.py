094b8cc88a91926c88e0920bef68570155ff5cd3
Leonid Stoyko <gribojor@gmail.com>
2026-05-19 19:59:20 +0500

    optimized post_detail

--- blog/views.py
+++ blog/views.py
@@ -79,8 +79,13 @@ def index(request):
 
 def post_detail(request, slug):
 
-    post = Post.objects.get(slug=slug)
-    comments = Comment.objects.filter(post=post)
+    post = (
+        Post.objects.select_related('author')
+        .with_annotated_tags()
+        .annotate(likes_count=Count('likes'))
+        .get(slug=slug)
+    )
+    comments = Comment.objects.filter(post=post).select_related('author')
     serialized_comments = []
     for comment in comments:
         serialized_comments.append({
@@ -89,8 +94,6 @@ def post_detail(request, slug):
             'author': comment.author.username,
         })
 
-    likes = post.likes.all()
-
     related_tags = post.tags.all()
 
     serialized_post = {
@@ -98,7 +101,7 @@ def post_detail(request, slug):
         'text': post.text,
         'author': post.author.username,
         'comments': serialized_comments,
-        'likes_amount': len(likes),
+        'likes_amount': post.likes_count,
         'image_url': post.image.url if post.image else None,
         'published_at': post.published_at,
         'slug': post.slug,
